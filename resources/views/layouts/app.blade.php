c06c8351a96a818229bf282d807c8607ca652a75
DevinNorgarb <dnorgarb@gmail.com>
2017-03-02 23:34:18 +0200

    APP_DEBUG=true

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SB Package Manager') }}</title>
+    <title> SB Package Manager</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -42,7 +42,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        SB Admin
                     </a>
                     @if (Auth::user())
                       <a class="navbar-brand" href="{{ route('packages') }}">
