d2a17f454d132c4530d57647188b8e2bcef81cad
DevinNorgarb <dnorgarb@gmail.com>
2017-05-26 00:32:21 +0200

    works now + added blade view

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -16,21 +16,28 @@ class EmailController extends Controller
 
     public function listener(Request $request)
     {
+      // dd($request->all());
         $vendor = $request->input("vendor");
         $email =  $request->input("email");
+        $data =  $request->input("data");
+        $data = json_decode($data);
         $name =  $request->input("user_name");
         $results_key =  $request->input("results_key");
-        // dd($results_key);
-        // var_dump(file_get_contents("http://10.0.0.17:8000/results.php?=".$results_key));
-        // die;
+        $submission =  $request->input("sub_id");
+
         $uri  = $request->input("uri");
         $host  = $request->input("host");
         $email_score_body = urldecode($request->input("email_score_body"));
         $vendor_email_score_body = urldecode($request->input("vendor_email_score_body"));
-        dd($vendor_email_score_body);
         $results =  urldecode($request->input("results"));
         $results = json_decode($results);
-
+        if (isset($submission)) {
+          $scores = DB::table('submissions_metrics')
+                    ->join('metrics', 'submissions_metrics.metric_id', '=', 'metrics.id')
+                    ->where('submissions_metrics.submission_id', '=', $submission)
+                    ->orderBy('metrics.id')
+                    ->get();
+        }
         if (isset($results)) {
         $result = [];
         foreach ($results as $vendor_selected) {
@@ -40,35 +47,68 @@ class EmailController extends Controller
         }
       }
 
+  // dd($data);
+
         $AirtableData = Airtable::getEntryByPackageName($vendor);
 
         if (isset($vendor)) {
+          if (!isset($scores)) {
             Mail::send("Email.EmailToVendor",
             [
               "email_score_body" => $vendor_email_score_body
             ], function ($message) use ($email, $AirtableData, &$vendor_email_score_body) {
-                if (isset($AirtableData[0]->{'Vendor Email'})) {
+              if (isset($AirtableData[0]->{'Vendor Email'})) {
 
-                    $date = date('H:i:s');
-                    $pdf =  PDF::loadHTML($vendor_email_score_body)->setPaper('a4' )
-                    ->setWarnings(true);
+                $date = date('H:i:s');
+                $pdf =  PDF::loadHTML($vendor_email_score_body)->setPaper('a4' )
+                ->setWarnings(true);
 
-                  $emails = explode(',', $AirtableData[0]->{'Vendor Email'});
+                $emails = explode(',', $AirtableData[0]->{'Vendor Email'});
 
-                    $message
-                    ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
-                    ->to($emails, "{$AirtableData[0]->CRM}")
-                    ->subject("SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}")
-                    ->attachData($pdf->output(), "SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}".".pdf");
-                    // die;
-                } else {
-                    $message
-                    ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
-                    ->to("dnorgarb@gmail.com", "No email record in DB for this referral")
-                    ->to("perry@smallbizcrm.com", "No email record in DB for this referral")
-                    ->subject("No vendor email record in DB for " . "{$AirtableData[0]->CRM}");
-                }
+                $message
+                ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
+                ->to($emails, "{$AirtableData[0]->CRM}")
+                ->subject("SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}")
+                ->attachData($pdf->output(), "SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}".".pdf");
+                // die;
+              } else {
+                $message
+                ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
+                ->to("dnorgarb@gmail.com", "No email record in DB for this referral")
+                ->to("perry@smallbizcrm.com", "No email record in DB for this referral")
+                ->subject("No vendor email record in DB for " . "{$AirtableData[0]->CRM}");
+              }
             });
+
+          } elseif (isset($scores)) {
+            Mail::send("Email.EmailToVendorFromUserEmail",
+            [
+              "scores" => $scores,
+              "data" => $data
+            ], function ($message) use ($email, $AirtableData, &$scores, $data) {
+              if (isset($AirtableData[0]->{'Vendor Email'})) {
+
+                // $date = date('H:i:s');
+                // $pdf =  PDF::loadHTML($scores)->setPaper('a4' )
+                // ->setWarnings(true);
+                //
+                $emails = explode(',', $AirtableData[0]->{'Vendor Email'});
+                $emails = "dnorgarb@gmail.com";
+                $message
+                ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
+                ->to($emails, "{$AirtableData[0]->CRM}")
+                ->subject("SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}");
+                // ->attachData($pdf->output(), "SmallBizCRM CRM Finder referral " . "{$AirtableData[0]->CRM}".".pdf");
+                // die;
+              } else {
+                $message
+                ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
+                ->to("dnorgarb@gmail.com", "No email record in DB for this referral")
+                ->to("perry@smallbizcrm.com", "No email record in DB for this referral")
+                ->subject("No vendor email record in DB for " . "{$AirtableData[0]->CRM}");
+              }
+            });
+          }
 
             if (isset($email)) {
               Mail::send("Email.ThankYouEmailToUser",
