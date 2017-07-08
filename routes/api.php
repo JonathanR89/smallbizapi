2d4c14ad40b214107227e3c38c92a2e65cde5e8e
DevinNorgarb <dnorgarb@gmail.com>
2017-07-08 18:07:48 +0200

    methods added

--- routes/api.php
+++ routes/api.php
@@ -2,30 +2,21 @@
 
 use Illuminate\Http\Request;
 
-// header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-// header('Access-Control-Allow-Headers: content-type');
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Credentials: true');
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-// Route::group(['middleware' => 'cors'], function () {
-Route::get('questions/{id}', 'QuestionnaireController@getMetrics');
-Route::get('categories', 'QuestionnaireController@getCategories');
-// Route::post('questionnaire', 'QuestionnaireController@saveSubmission');
-Route::post('questionnaire', 'QuestionnaireController@neilsway');
-// });
+// Route::group(['middleware' => 'api'], function () {
+    Route::get('questions/{id}', 'QuestionnaireController@getMetrics');
+    Route::get('categories', 'QuestionnaireController@getCategories');
+    Route::post('questionnaire', 'QuestionnaireController@saveSubmissionScores');
 
-// Route::group(['middleware' => 'cors'], function(){
-//     // Route::post('/api/chargeCustomer', 'Backend\PaymentController@chargeCustomer');
+// Route::post('questionnaire', 'QuestionnaireController@neilsway');
+
+// Route::group(['middleware' => 'Cors'], function () {
+    Route::get('save-submission-user', 'QuestionnaireController@saveSubmissionUser');
+    // Route::post('/api/chargeCustomer', 'Backend\PaymentController@chargeCustomer');
 // });
