a491010feed95a177a056fa422a4e8e8289c86d9
DevinNorgarb <dnorgarb@gmail.com>
2017-07-14 21:46:30 +0200

    web works again

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,12 +14,11 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
-        \Illuminate\Session\Middleware\StartSession::class
+      \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+      \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+      \App\Http\Middleware\TrimStrings::class,
+      \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+      \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -29,14 +28,14 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            // \Illuminate\Session\Middleware\StartSession::class
+          \App\Http\Middleware\EncryptCookies::class,
+          \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+          \Illuminate\Session\Middleware\StartSession::class,
+          // \Illuminate\Session\Middleware\AuthenticateSession::class,
+          \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+          \App\Http\Middleware\VerifyCsrfToken::class,
+          \Illuminate\Routing\Middleware\SubstituteBindings::class,
+          \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
