1e15b6057a9a584bd8f40b5bdc29b5142ba5b3d6
DevinNorgarb <dnorgarb@gmail.com>
2017-07-14 17:54:37 +0200

    seeder ADDED

---
+++ database/seeds/IndustryTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SubmissionIndustry;
+
+class IndustryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $industries = collect([
+        "Automotive Industry",
+        "Beauty and Wellness Industry",
+        "Childcare/Aftercare/Creche",
+        "Construction and Architects",
+        "Distributors",
+        "Education",
+        "Financial Services",
+        "Franchises",
+        "Freelancers",
+        "Gambling/Casino",
+        "Governments",
+        "IT Dealers &amp; Service Providers",
+        "Insurance Industry",
+        "Legal Practices &amp; Attorneys",
+        "Media &amp; Entertainment",
+        "Medical &amp; Healthcare Profession",
+        "Moving &amp; Storage",
+        "Multi-Level Marketing (MLM)",
+        "Newspapers &amp; Publishing",
+        "NGO/NPO/Charities",
+        "Pharmaceutical",
+        "Real Estate",
+        "Recruiting &amp; Staffing",
+        "Restaurants",
+        "Travel Agencies/Tourism",
+      ]);
+        $industries->each(function ($industry) {
+            SubmissionIndustry::create([ "industry_name" => $industry]);
+        });
+    }
+}
