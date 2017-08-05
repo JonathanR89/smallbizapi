84315c115cb5e81bba46831e1b304884548a6071
DevinNorgarb <dnorgarb@gmail.com>
2017-08-05 14:36:37 +0200

    revert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Small Biz Admin YO
+                    Small Biz Admin
                 </div>
             </div>
         </div>
