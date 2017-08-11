65d1aa65b4ea6ad39bbdb204125ac7a9b54921ad
DevinNorgarb <dnorgarb@gmail.com>
2017-08-11 18:46:30 +0200

    consultants table

---
+++ database/migrations/2017_08_11_164140_create_consultants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConsultantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consultants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('company')->nullable();
+            $table->string('email');
+            $table->string('profile_pic')->nullable();
+            $table->string('country')->nullable();
+            $table->string('phone_number');
+            $table->text('description')->nullable();
+            $table->integer('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consultants');
+    }
+}
