03734151570cb07a46a7f57d059eeff57eb8014a
DevinNorgarb <dnorgarb@gmail.com>
2017-08-11 18:18:15 +0200

    php artisan cache:clear

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -8,8 +8,8 @@
 @task('deploy-staging', ['on' => 'web'])
 cd /var/www/html/SmallBizAPI/
 git pull origin
-rm -rf vendor/
 php artisan cache:clear
+rm -rf vendor/
 composer dump-autoload
 composer install
 @endtask
