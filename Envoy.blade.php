d2f070f2c4b1cdab80b77512c180e31f18d39316
DevinNorgarb <dnorgarb@gmail.com>
2017-08-18 23:31:40 +0200

    frontend api auto deploy small biz crm

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['web' => 'root@devserv.devswebdev.com'])
+@servers(['web' => 'root@devserv.devswebdev.com' , 'production' => 'smallbiz@smallbizcrm.com'])
 
 @story('deploy')
     git
@@ -6,15 +6,28 @@
 @endstory
 
 @task('deploy-staging', ['on' => 'web'])
-cd /var/www/html/SmallBizAPI/
-git pull origin
-php artisan cache:clear
-composer clear-cache
-composer dump-autoload
-rm -rf vendor/
-composer install
-composer update
-rm -rf node_modules/
-npm install
-php artisan migrate
+  cd /var/www/html/SmallBizAPI/
+  git pull origin
+  php artisan cache:clear
+  composer clear-cache
+  composer dump-autoload
+  rm -rf vendor/
+  composer install
+  composer update
+  rm -rf node_modules/
+  npm install
+  php artisan migrate
+@endtask
+
+@task('deploy-production', ['on' => 'production'])
+  cd /home/smallbiz/public_html/packagemanager
+  ls
+  git pull origin master
+  php artisan cache:clear
+  composer clear-cache
+  composer dump-autoload
+  rm -rf vendor/
+  composer install
+  composer update
+  php artisan migrate
 @endtask
