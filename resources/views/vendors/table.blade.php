67d72c85b02976f7a0e315b87d736d1b8f00e705
DevinNorgarb <dnorgarb@gmail.com>
2017-09-02 21:26:42 +0200

    d

--- resources/views/vendors/table.blade.php
+++ resources/views/vendors/table.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  {{-- <div class="container"> --}}
-  {{-- <div class="row"> --}}
   <div class="panel panel-default">
     <div class="table-responsive">
-      {{-- <div class="panel-heading">Dashboard</div> --}}
-      {{-- <div class="panel-body"> --}}
+
       <a href="{{ url('all-vendors') }}"></a>
       <table class="table table-hover">
         <thead>
