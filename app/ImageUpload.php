50cc2bbbb94310c4cfaf9a01407bf7c4805c77f2
DevinNorgarb <dnorgarb@gmail.com>
2017-09-08 19:38:10 +0200

    upload

--- app/ImageUpload.php
+++ app/ImageUpload.php
@@ -6,5 +6,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImageUpload extends Model
 {
-    //
+    protected $casts = [
+      'original_filename' => 'string',
+      'original_filepath' => 'string',
+      'original_filedir' => 'string',
+      'original_extension' => 'string',
+      'original_mime' => 'string',
+      'original_filesize' => 'integer',
+      'original_width' => 'integer',
+      'original_height' => 'integer',
+      'path' => 'string',
+      'dir' => 'string',
+      'filename' => 'string',
+      'basename' => 'string',
+      'exif' => 'array',
+  ];
+
+  /**
+   * The keys used in thumbnail.
+   *
+   * @var array
+   */
+  protected $thumbnailKeys = [
+      'path' => 'string',
+      'dir' => 'string',
+      'filename' => 'string',
+      'filepath' => 'string',
+      'filedir' => 'string',
+      'width' => 'integer',
+      'height' => 'integer',
+      'filesize' => 'integer',
+      'is_squared' => 'boolean',
+  ];
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'original_filename', 'original_filepath', 'original_filedir',
+      'original_extension', 'original_mime', 'original_filesize',
+      'original_width', 'original_height',
+      'path', 'dir', 'filename', 'basename',
+      'exif',
+  ];
 }
