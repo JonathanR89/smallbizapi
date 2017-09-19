9e3f6795a60336685a1bb31358a64a9091300d71
DevinNorgarb <dnorgarb@gmail.com>
2017-09-19 08:53:21 +0200

    null vlidate

--- app/Jobs/SendFollowUpConsultantFinderEmail.php
+++ app/Jobs/SendFollowUpConsultantFinderEmail.php
@@ -37,7 +37,7 @@ class SendFollowUpConsultantFinderEmail implements ShouldQueue
            $message
            ->from("perry@smallbizcrm.com", "SmallBizCRM.com")
            ->to("devin@smallbizcrm.com", "SmallBizCRM.com")
-            ->to($this->userData['email'], $this->userData['name'])
+           ->to($this->userData['email'] ?: "dnorgarb@gmail.com", $this->userData['name'] ?: "dnorgarb@gmail.com")
            ->subject("CRM Consulting Enquiry");
        });
     }
