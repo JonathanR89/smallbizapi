0b59812a3c919525ffd723a4705608474debce60
DevinNorgarb <dnorgarb@gmail.com>
2017-09-21 23:12:44 +0200

    works

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -278,13 +278,19 @@ class QuestionnaireController extends Controller
                       "airtableData" => $vendor,
                       "data" => $row,
                     ];
-                    $score =  max($max, intval($row->score));
+                    $max =  max($max, intval($row->score));
+
+                    $score = SubmissionsPackage::where(['submission_id' => $submission_id, 'package_id' =>  $row->id])->get()->toArray();
+                    // if ($score) {
+                    //     dd($score[0]['score']);
+                    //     // dd($score);
+                    // }
                     UserResult::create([
                       "submission_id" => $submission_id,
                       "user_id" => $user_id,
                       "package_name" => $row->name,
                       "package_id" => $row->id,
-                      "score" => $score,
+                      "score" => $score[0]['score'],
                     ]);
                 }
             }
