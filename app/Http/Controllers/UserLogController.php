30c6c45556debdb32ee326cec1e544aa3e3c2a00
DevinNorgarb <dnorgarb@gmail.com>
2017-09-24 18:14:08 +0200

    works

--- app/Http/Controllers/UserLogController.php
+++ app/Http/Controllers/UserLogController.php
@@ -12,7 +12,7 @@ use App\Http\Traits\Airtable;
 use App\SubmissionPriceRange;
 use App\ImageUpload as ImageUploadModel;
 
-class VendorController extends Controller
+class UserLogController extends Controller
 {
     public function logUser(Request $request)
     {
