0b3b1bdc1c52b34852183b08c121c927b21a0023
DevinNorgarb <dnorgarb@gmail.com>
2017-10-07 07:09:33 +0200

    daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
             $schedule->command('queue:listen')->everyMinute()->withoutOverlapping();
         } elseif (env('APP_ENV') == 'staging') {
             $schedule->command('airtable:seed')->everyMinute();
-            $schedule->command('send:report')->hourly();
+            $schedule->command('send:report')->daily();
         }
         $schedule->command('exports:clear')->hourly();
     }
