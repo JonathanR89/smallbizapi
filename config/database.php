bec35385542a703c5adeee152c45744e0c76ac83
DevinNorgarb <dnorgarb@gmail.com>
2017-11-01 22:51:08 +0200

    dbconnection

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
 
         'stage' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'devserv.devswebdev.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
