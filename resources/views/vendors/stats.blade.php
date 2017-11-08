28baf2cce8c340a72d140e02d8b08cf60ffe6f52
DevinNorgarb <dnorgarb@gmail.com>
2017-11-09 00:27:42 +0200

    wip

--- resources/views/vendors/stats.blade.php
+++ resources/views/vendors/stats.blade.php
@@ -5,17 +5,13 @@
         <div class="col-md-8 col-md-offset-2 table-responsive">
 
           <div class="panel-heading">
-
+            <h2>Vendors without data</h2>
           </div>
 
           <div class="panel-body">
             <div class="row">
             @foreach ($info as $key => $vendors)
-              @if ( $vendors->count() > 0)
-                {{-- @php
-                  dd($vendor);
-                @endphp --}}
-              @endif
+
               {{-- <a href="{{ route('show_vendor_incomplete', [$vendor['id']])}}"></a> --}}
                 <div class="col-md-4">
                   <div class="alert alert-{{ collect(['danger', 'success', 'warning'])->random() }}">
@@ -32,7 +28,30 @@
               @endphp --}}
 
             @endforeach
-          </div>
+              {{-- <a href="{{ route('show_vendor_incomplete', [$vendor['id']])}}"></a> --}}
+                <div class="col-md-4" data-toggle="collapse">
+                  <div id="collapse1" class="panel-collapse collapse">
+
+                  <div id="demo"  class="alert  alert-{{ collect(['danger', 'success', 'warning'])->random() }}">
+                    <a data-toggle="collapse" href="#collapse1">
+                      <strong>Missing Images</strong> Missing: {{ collect($infoimage)->count() }}
+                    </a>
+
+                      @foreach ($infoimage as $vendor)
+                        <div id="collapse1" class="panel-collapse collapse">
+
+
+                          <a href="{{ url('vendor/'.$vendor->id.'/show') }}">
+                            {{ $vendor->name }} <br>
+                          </a>
+
+                        </div>
+                      @endforeach
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div class="">
 
             </div>
