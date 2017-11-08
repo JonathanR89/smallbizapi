7e1fc1348238a8fab88d631324c1e934ca77f724
DevinNorgarb <dnorgarb@gmail.com>
2017-11-08 22:17:25 +0200

    laavcharts

---
+++ resources/views/vendors/stats.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="col-md-8 col-md-offset-2 table-responsive">
+
+          <div class="panel-heading">
+
+          </div>
+
+          <div class="panel-body">
+
+        </div>
+        </div>
+
+      </div>
+
+
+@endsection
