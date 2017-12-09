063f35c3906d01cec3e09e8648af2c504e3baaa4
DevinNorgarb <dnorgarb@gmail.com>
2017-12-09 19:01:55 +0200

    config

--- config/analytics.php
+++ config/analytics.php
@@ -1,25 +1,32 @@
 <?php
 
+
+$googleAnalyJsonFilePath = storage_path('app/analytics/smallbizcrm-f386b5059421.json');
+$viewID = '161456901';
+
+if (env('APP_ENV') == 'production') {
+   $googleAnalyJsonFilePath = storage_path('app/analytics/smallbizcrm-188215.json');
+   $viewID = '10812592';
+}
+
+
 return [
 
     /*
      * The view id of which you want to display data.
      */
-    'view_id' => '10812592',
-
-// 161456901
+    'view_id' => $viewID,
     /*
      * Path to the client secret json file. Take a look at the README of this package
      * to learn how to get this file.
      */
-    //  'service_account_credentials_json' => storage_path('app/analytics/smallbizcrm-f386b5059421.json'),
-    'service_account_credentials_json' => storage_path('app/analytics/smallbizcrm-188215.json'),
+     'service_account_credentials_json' => $googleAnalyJsonFilePath,
 
     /*
      * The amount of minutes the Google API responses will be cached.
      * If you set this to zero, the responses won't be cached at all.
      */
-    'cache_lifetime_in_minutes' => 1,
+    'cache_lifetime_in_minutes' => 10,
 
     /*
      * Here you may configure the "store" that the underlying Google_Client will
@@ -29,6 +36,6 @@ return [
      * Optional parameters: "lifetime", "prefix"
      */
     'cache' => [
-        'store' => 'file',
+        'store' => 'database',
     ],
 ];
